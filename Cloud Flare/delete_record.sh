92e4c90cb77d3b3dc8099dc30fe86e1f13f40a7b
Eibad <45168328+Eibad@users.noreply.github.com>
2024-10-10 14:33:36 +0500

    Add files via upload

---
+++ Cloud Flare/delete_record.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Cloudflare API URL and token
+zone_id="0ddcd29980f96e45b512331c6e9f2e92"
+auth_token="Ntw1r3du-etaB0Kq9F3WJakF3oh-I1koTXSL73Pm"
+
+# Read each line (ID) from the ids.txt file and send DELETE request
+while IFS= read -r id; do
+    # Make DELETE request to Cloudflare API for each ID
+    response=$(curl --location --request DELETE "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$id" \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Bearer $auth_token")
+    
+    # Check if the deletion was successful
+    if echo "$response" | grep -q '"success":true'; then
+        echo "Successfully deleted record with ID: $id"
+    else
+        echo "Failed to delete record with ID: $id"
+        echo "Response: $response"
+    fi
+done < ids.txt
+
---
+++ Cloud Flare/get_ids.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Cloudflare API URL
+url="https://api.cloudflare.com/client/v4/zones/0ddcd29980f96e45b512331c6e9f2e92/dns_records?per_page=400"
+
+# Cloudflare API Token
+auth_token="Ntw1r3du-etaB0Kq9F3WJakF3oh-I1koTXSL73Pm"
+
+# Curl command to fetch DNS records
+response=$(curl --location "$url" \
+--header "Authorization: Bearer $auth_token" \
+--header "Content-Type: application/json")
+
+# Use jq to filter the result where "content" is "dev.galaxefi.com" and get the "id"
+ids=$(echo "$response" | jq -r '.result[] | select(.content == "dev.galaxefi.com") | .id')
+
+# Check if any IDs were found
+if [ -n "$ids" ]; then
+    # Store the IDs in a file
+    echo "$ids" > ids.txt
+    echo "IDs with content 'dev.galaxefi.com' have been saved to ids.txt"
+else
+    echo "No records found with content 'dev.galaxefi.com'"
+fi
+
